75f4d7332142c56c4b9c30e76195a89d811fad9a
mofe123 <mofe12>
2023-06-23 00:46:49 -0400

    Initial Commit

---
+++ Nudge/NudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NudgeApp.swift
+//  Nudge
+//
+//  Created by Eyimofe Oladipo on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct NudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
